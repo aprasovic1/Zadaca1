6c75f55893cacd3fb7c0daf13fb55cbe6c10e6f9
aprasovic1 <aprasovic1@etf.unsa.ba>
2022-11-12 12:24:37 +0100

    pocetak

---
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Stack;
+
+public class ExpressionEvaluator {
+    double evaluate(String exp) {
+        Stack<String> operator=new Stack<String>();
+        Stack<Double> operand=new Stack<Double>();
+
+
+
+        return 0;
+    }
+}
