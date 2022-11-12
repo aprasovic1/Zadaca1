cf68982ffe4bd4acd459dc54ba26f1c2e75dc8b6
aprasovic1 <aprasovic1@etf.unsa.ba>
2022-11-12 13:46:58 +0100

    ne radi na terminalu a radi u IDEA

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,12 @@
 package ba.unsa.etf.rpr;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        ExpressionEvaluator ex=new ExpressionEvaluator();
+       System.out.println(ex.evaluate("1"));
     }
 }
--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -1,14 +1,37 @@
 package ba.unsa.etf.rpr;
 
 import java.util.Stack;
+import java.util.Scanner;
 
 public class ExpressionEvaluator {
     double evaluate(String exp) {
-        Stack<String> operator=new Stack<String>();
-        Stack<Double> operand=new Stack<Double>();
+        Stack<String> operator = new Stack<String>();
+        Stack<Double> operand = new Stack<Double>();
+//System.out.println(exp.charAt(1));
+
+        for (int i = 0; i < exp.length(); i++) {
+            String znak ;
+            if (exp.charAt(i) == ' ') continue;
+            else
+                znak = String.valueOf(exp.charAt(i));
+            if (znak.equals("(")) ;
+            else if (znak.equals("+")) operator.push(znak);
+            else if (znak.equals("-")) operator.push(znak);
+            else if (znak.equals("*")) operator.push(znak);
+            else if (znak.equals("/")) operator.push(znak);
+            else if (znak.equals(")")) {
+                String op = operator.pop();
+                double broj = operand.pop();
+                if (op.equals("+")) broj = operand.pop() + broj;
+                else if (op.equals("-")) broj = operand.pop() - broj;
+                else if (op.equals("*")) broj = operand.pop() * broj;
+                else if (op.equals("/")) broj = operand.pop() / broj;
+                operand.push(broj);
+            } else operand.push(Double.parseDouble(znak));
+
+        }
 
 
-
-        return 0;
+        return operand.pop();
     }
 }
