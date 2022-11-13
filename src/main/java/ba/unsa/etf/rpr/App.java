065a8384372cbad5511ae3823ec5bee52e775e3f
aprasovic1 <aprasovic1@etf.unsa.ba>
2022-11-13 13:17:44 +0100

    Zavrsena validacija i JavaDoc komentari

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,18 +1,46 @@
 package ba.unsa.etf.rpr;
 
 
+import java.util.Arrays;
+/**
+ * Class App only has method
+ */
+public class App {
+    /**
+     *  Method Main is parsing array of arithmetic operators and numbers from console, saved in String[] args,
+     *  validating it, and building a string of those operators and numbers so that each
+     *  character or operation "sqrt" is separated by a space.
+     *
+     *  Then it prints a computed value of given expression.
+     *
+     */
+    public static void main(String[] args) {
+        String operacije = new String("+-*/");
 
+        // System.out.println("'"+args[0]+"'"+args[args.length-1]+"'");
+        //( sqrt ( 4 ) + ( 7 * 2.54 ) )
+        if (!args[0].equals("(") || !args[args.length - 1].equals(")"))
+            throw new RuntimeException("Izraz nije validan");
 
-public class App
-{
-    public static void main( String[] args )
-    {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("(") && (operacije.contains(args[i + 1]) ))
+                throw new RuntimeException("Izraz nije validan");
+            else if(operacije.contains(args[i]) &&operacije.contains(args[i+1]))
+                throw new RuntimeException("Izraz nije validan");
+            else if(!args[i].equals("(")&&!args[i].equals(")") &&!operacije.contains(args[i]) && !args[i].equals("sqrt")) {
+                try {
+                    Double.parseDouble(args[i]);
+                } catch (NumberFormatException nfe) {
+                    throw new RuntimeException("Izraz nije validan");
+                }
+            }
 
+        }
 
-        StringBuilder s1=new StringBuilder();
+        StringBuilder s1 = new StringBuilder();
 
         for (String arg : args) s1.append(arg).append(' ');
-        ExpressionEvaluator ex= new ExpressionEvaluator();
+        ExpressionEvaluator ex = new ExpressionEvaluator();
         System.out.println(ex.evaluate(s1.toString()));
     }
 }
--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -3,7 +3,21 @@ package ba.unsa.etf.rpr;
 import java.util.Stack;
 import java.util.Scanner;
 
+
+/**
+ * Class ExpressionEvaluator has only one
+ * method "evaluate"
+ *
+ */
 public class ExpressionEvaluator {
+
+    /**
+     * <p>Method evaluate computes the value of an arithmetic expression</p>
+     * @param exp arithmetic expression as a string
+     * @return computed value of an arithmetic expression
+     *
+     *
+     * */
     double evaluate(String exp) {
         Stack<String> operator = new Stack<String>();
         Stack<Double> operand = new Stack<Double>();
@@ -12,13 +26,18 @@ public class ExpressionEvaluator {
         for (int i = 0; i < exp.length(); i++) {
             String znak ;
             if (exp.charAt(i) == ' ') continue;
-            else
-                znak = String.valueOf(exp.charAt(i));
+            else {
+                int razmak = exp.indexOf(' ', i);
+                znak = exp.substring(i,razmak);
+                i=razmak;
+            }
+
             if (znak.equals("(")) ;
             else if (znak.equals("+")) operator.push(znak);
             else if (znak.equals("-")) operator.push(znak);
             else if (znak.equals("*")) operator.push(znak);
             else if (znak.equals("/")) operator.push(znak);
+            else if (znak.equals("sqrt")) operator.push(znak);
             else if (znak.equals(")")) {
                 String op = operator.pop();
                 double broj = operand.pop();
@@ -26,6 +45,7 @@ public class ExpressionEvaluator {
                 else if (op.equals("-")) broj = operand.pop() - broj;
                 else if (op.equals("*")) broj = operand.pop() * broj;
                 else if (op.equals("/")) broj = operand.pop() / broj;
+                else if (op.equals("sqrt")) broj = Math.sqrt(broj);
                 operand.push(broj);
             } else operand.push(Double.parseDouble(znak));
 
