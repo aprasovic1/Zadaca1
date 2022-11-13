9f8095f97a4c7ca661fa9b7ef19e497ea1c62f9a
aprasovic1 <aprasovic1@etf.unsa.ba>
2022-11-13 12:07:30 +0100

    Radi evaluate

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,14 +1,18 @@
 package ba.unsa.etf.rpr;
 
 
-public class App 
+
+
+public class App
 {
     public static void main( String[] args )
     {
 
-        ExpressionEvaluator ex;
-        ex = new ExpressionEvaluator();
-        for(int i=0;i<args.length;i++)
-        System.out.println(args[i]);
+
+        StringBuilder s1=new StringBuilder();
+
+        for (String arg : args) s1.append(arg).append(' ');
+        ExpressionEvaluator ex= new ExpressionEvaluator();
+        System.out.println(ex.evaluate(s1.toString()));
     }
 }
